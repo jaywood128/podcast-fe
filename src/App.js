59b806327b6e541b116901ba5817d7b2828edbc1
Johnathon Wood <jaywood128@gmail.com>
2021-03-06 14:01:25 -0500

    Add eslint, react bootstrap, prettier

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <h1>Hello World!</h1>
+      <h1>Hello World!</h1>
     </div>
   );
 }
